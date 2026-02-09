f69098eb74d914893eb01fa1fd9d9e6c29d42c0e
ajassi9751 <ajassi9751@gmail.com>
2026-02-09 21:41:28 +0000

    Build.rs can now compile all c files!

--- build.rs
+++ build.rs
@@ -1,3 +1,39 @@
+use cc::Build;
+use std::fs::read_dir;
+use std::process::exit;
+
 fn main () {
-    cc::Build::new().file("c/main.c").compile("main");
+    let mut filenames: Vec<String> = Vec::new();
+    match read_dir("c/") {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(v) => {
+                        let filename = v.file_name().into_string().expect("Issue reading filename");
+                        // Don't compile header files
+                        if filename.ends_with(".c") {
+                            filenames.push(filename);
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Illegal file/directory access error: {}", e);
+                        exit(1);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Illegal file/directory access error: {}", e);
+            exit(1);
+        }
+    }
+    let mut compilenames: Vec<String> = filenames.clone();
+    for name in compilenames.iter_mut() {
+        // Removes .c file extension
+        name.truncate(name.len() - 2);
+    }
+    for (src, libname) in filenames.iter().zip(compilenames.iter()) {
+        Build::new().file(format!("c/{}", src)).compile(libname.as_str());
+    }
+    // Build::new().file("c/main.c").compile("main");
 }
