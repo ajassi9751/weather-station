c8f92e2e87c3d4909bd3f18b490db5a8b3750229
ajassi9751 <ajassi9751@gmail.com>
2026-02-09 17:58:45 -0500

    Made build recursively go through directories

--- build.rs
+++ build.rs
@@ -2,17 +2,37 @@ use cc::Build;
 use std::fs::read_dir;
 use std::process::exit;
 
-fn main () {
+fn main() {
     let mut filenames: Vec<String> = Vec::new();
-    match read_dir("c/") {
+    get_file_paths(&mut filenames, "c/");
+    let mut compilenames: Vec<String> = Vec::new();
+    get_file_names(&mut compilenames, "c/");
+    for name in compilenames.iter_mut() {
+        // Removes .c file extension
+        name.truncate(name.len() - 2);
+    }
+    for (src, libname) in filenames.iter().zip(compilenames.iter()) {
+        Build::new().file(src).compile(libname.as_str());
+    }
+    // Build::new().file("c/main.c").compile("main");
+}
+
+fn get_file_paths(filenames: &mut Vec<String>, path: &str) {
+    match read_dir(path) {
         Ok(entries) => {
             for entry in entries {
                 match entry {
                     Ok(v) => {
-                        let filename = v.file_name().into_string().expect("Issue reading filename");
-                        // Don't compile header files
-                        if filename.ends_with(".c") {
-                            filenames.push(filename);
+                        if v.file_type().expect("Error getting file type").is_dir() {
+                            get_file_names(filenames, v.path().display().to_string().as_str());
+                        } else {
+                            let filename =
+                                v.file_name().into_string().expect("Issue reading filename");
+                            // Don't compile header files
+                            if filename.ends_with(".c") {
+                                // Double check this
+                                filenames.push(format!("{}/{}", path, filename));
+                            }
                         }
                     }
                     Err(e) => {
@@ -27,13 +47,36 @@ fn main () {
             exit(1);
         }
     }
-    let mut compilenames: Vec<String> = filenames.clone();
-    for name in compilenames.iter_mut() {
-        // Removes .c file extension
-        name.truncate(name.len() - 2);
+}
+
+fn get_file_names(filenames: &mut Vec<String>, path: &str) {
+    match read_dir(path) {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(v) => {
+                        if v.file_type().expect("Error getting file type").is_dir() {
+                            get_file_names(filenames, v.path().display().to_string().as_str());
+                        } else {
+                            let filename =
+                                v.file_name().into_string().expect("Issue reading filename");
+                            // Don't compile header files
+                            if filename.ends_with(".c") {
+                                // Double check this
+                                filenames.push(filename);
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Illegal file/directory access error: {}", e);
+                        exit(1);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Illegal file/directory access error: {}", e);
+            exit(1);
+        }
     }
-    for (src, libname) in filenames.iter().zip(compilenames.iter()) {
-        Build::new().file(format!("c/{}", src)).compile(libname.as_str());
-    }
-    // Build::new().file("c/main.c").compile("main");
 }
