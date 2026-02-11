9112260369f3466743161a0c08363639f481c959
ajassi9751 <ajassi9751@gmail.com>
2026-02-11 18:47:54 -0500

    Fixed formatting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod c;
 mod io;
 // use wiringpi::pin::Value::{self, High, Low};
-use std::{thread, time};
 use dht_mmap_rust::{Dht, DhtType};
+use std::{thread, time};
 // Use: cargo build --features wiringpi/development, to build in development but it wont interact with gpio
 // To install wiring pi use wget https://github.com/WiringPi/WiringPi/releases/download/3.18/wiringpi_3.18_arm64.deb
 // Then use sudo apt install ./[filename]
@@ -13,17 +13,23 @@ use dht_mmap_rust::{Dht, DhtType};
 // Writing to files will not use buffered writes as every hour makes them worthless
 fn main() {
     let headers: Vec<String> = vec![String::from("Temp"), String::from("Coolness")];
-    let body: Vec<Vec<String>> = vec![vec![String::from("Nice"), String::from("and Cool")], vec![String::from("Really"), String::from("Cool")]];
+    let body: Vec<Vec<String>> = vec![
+        vec![String::from("Nice"), String::from("and Cool")],
+        vec![String::from("Really"), String::from("Cool")],
+    ];
     let mut mycsv = io::csv::csv::new(headers, body);
     let _ = mycsv.save_to_file("data/my.csv");
-    let _ = mycsv.write_new_row("data/my.csv", vec![String::from("really"), String::from("really")]);
+    let _ = mycsv.write_new_row(
+        "data/my.csv",
+        vec![String::from("really"), String::from("really")],
+    );
     // let pi = wiringpi::setup();
     // let pin = pi.input_pin(6);
     // loop {
     //     thread::sleep(time::Duration::from_millis(2000));
     //     let value = pin.digital_read();
     //     match value {
-    //         High => {println!("High");}, 
+    //         High => {println!("High");},
     //         Low => {println!("Low");},
     //     }
     // }
@@ -46,12 +52,13 @@ fn main() {
     // println!("{}", c::add::add(1,2));
 }
 
-fn get_dht (pin: usize) -> Dht {
+fn get_dht(pin: usize) -> Dht {
     match Dht::new(DhtType::Dht11, pin) {
         Ok(v) => v,
-        Err(e) => {
+        Err(_e) => {
             println!("Error accessing dht");
             get_dht(pin)
-        },
+        }
     }
 }
+
