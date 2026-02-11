28fa6eeb3892ea00e6ce3bda12ecf080e8d3e3bc
ajassi9751 <ajassi9751@gmail.com>
2026-02-10 19:07:33 -0500

    Fixed the extra comma issue on writing a whole file

--- src/io/csv.rs
+++ src/io/csv.rs
@@ -3,7 +3,7 @@ use std::fs::write;
 
 #[allow(non_camel_case_types)]
 pub struct csv {
-    headers: Vec<String>, // Vec of strings is kind of sad
+    headers: Vec<String>,   // Vec of strings is kind of sad
     body: Vec<Vec<String>>, // Vec of Vec of strings is even sadder
 }
 
@@ -11,43 +11,54 @@ pub struct csv {
 // Maybe a weelky new file?
 // Need to also get the date and time
 impl csv {
-    pub fn new () -> csv {
-        csv {headers:Vec::new(), body:Vec::new()}
+    pub fn new() -> csv {
+        csv {
+            headers: Vec::new(),
+            body: Vec::new(),
+        }
     }
-    pub fn save_to_file (&self, file_path: &str) -> Result<(),IoError> {
+    pub fn save_to_file(&self, file_path: &str) -> Result<(), IoError> {
         let mut contents: String = String::from("");
         for header in &self.headers {
-            if !(contents=="") {
+            // The reason I don't add the commas after is because it will mess it up at the end of
+            // a row
+            if !(contents == "") {
                 contents = contents + "," + header.as_str();
-            }
-            else {
+            } else {
                 contents = contents + header.as_str();
             }
         }
         contents = contents + "\n";
         for i in &self.body {
+            let mut is_first_iter = true;
             for v in i {
-                contents = contents + "," + v.as_str();
+                if !is_first_iter {
+                    contents = contents + "," + v.as_str();
+                } else {
+                    contents = contents + v.as_str();
+                    is_first_iter = false;
+                }
             }
             contents = contents + "\n";
         }
         match write(file_path, contents) {
             Ok(()) => return Ok(()),
-            Err(_e) => {return Err(IoError{});},
+            Err(_e) => {
+                return Err(IoError {});
+            }
         }
     }
-    pub fn write_new_row (&mut self, file_path: &String, row: Vec<String>) -> std::io::Result<()> {
+    pub fn write_new_row(&mut self, file_path: &String, row: Vec<String>) -> std::io::Result<()> {
         let file_contents = read_to_string(file_path)?;
         let mut contents = file_contents;
         let mut is_first_iter = true;
         self.body.push(row.clone());
         for string in row {
-            if is_first_iter {
+            if !is_first_iter {
+                contents = contents + "," + string.as_str();
+            } else {
                 contents = contents + string.as_str();
                 is_first_iter = false;
-            }
-            else {
-                contents = contents + "," + string.as_str();
             }
         }
         write(file_path, contents)?;
@@ -58,3 +69,4 @@ impl csv {
 // Rust says that the struct is never constructed even though it is
 #[allow(unused)]
 pub struct IoError;
+
