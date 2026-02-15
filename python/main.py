fb19a1a24c6806567344bdcba5667d4877eb0683
ajassi9751 <ajassi9751@gmail.com>
2026-02-14 19:02:51 -0500

    Maybe fixed python

--- python/main.py
+++ python/main.py
@@ -1,22 +1,23 @@
-#/usr/bin/env python
+#!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
 import time
 
-LedPin = 3    # pin11
+LedPin = 2
 
-GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
-GPIO.setup(LedPin, GPIO.OUT)   # Set pin mode as output
-GPIO.output(LedPin, GPIO.HIGH) # Set pin to high(+3.3V) to off the led
+GPIO.setmode(GPIO.BCM)       # Use BCM numbering
+GPIO.setup(LedPin, GPIO.OUT) # Set pin mode as output
+GPIO.output(LedPin, GPIO.HIGH) # Set pin to high(+3.3V) to turn off the LED
 
 try:
-	while True:
-		print ('...led on')
-		GPIO.output(LedPin, GPIO.LOW)  # led on
-		time.sleep(0.5)
-		print ('led off...')
-		GPIO.output(LedPin, GPIO.HIGH) # led off
-		time.sleep(0.5)
-except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the flowing code will be  executed.
-	GPIO.output(LedPin, GPIO.HIGH)     # led off
-	GPIO.cleanup()                     # Release resource
+    while True:
+        print('...led on')
+        GPIO.output(LedPin, GPIO.LOW)  # LED on
+        time.sleep(0.5)
+        print('led off...')
+        GPIO.output(LedPin, GPIO.HIGH) # LED off
+        time.sleep(0.5)
+except KeyboardInterrupt:  # When 'Ctrl+C' is pressed
+    GPIO.output(LedPin, GPIO.HIGH)     # LED off
+    GPIO.cleanup()                     # Release resources
+
