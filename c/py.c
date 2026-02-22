6d7c5d57cccac399d1e63f3bb3689717db1263b0
ajassi9751 <ajassi9751@gmail.com>
2026-02-22 12:29:18 -0500

    Finished c translation

--- c/py.c
+++ c/py.c
@@ -1,10 +1,20 @@
+#include <math.h>
 #include<unistd.h>
 #include<stdio.h>
 #include<wiringPi.h>
 
 #define channel 17
+// This macro replicates the python y = x[0:1] syntax
+// It also keeps list1i in its own scope so it doesn't cause redefinition issues
+#define LIST_SEGMENT(list1, list2, range1, range2) {\
+	int list1i = 0;\
+		for (int i = range1; i < range2 - 1; i++) {\
+			list1[list1i] = list2[i];\
+			list1i++;\
+		}\
+}
 
-int main () {
+int print_temp_humidity () {
 	printf("Start\n");
 	int data[100];
 	int j = 0;
@@ -42,41 +52,40 @@ int main () {
 		}
 		j++;
 	}
-	printf("sensor is working.");
+	printf("sensor is working.\n");
 	for (int i = 0; i < datai-1; i++) {
-		printf("%d", data[i]);
+		printf("%d\n", data[i]);
 	}
+	// Please no buffer overflow
 	int humidity_bit[8];
-	for (int i = 0; i < datai-1; i++) {
-
+	LIST_SEGMENT(humidity_bit, data, 0, 8);
+	int humidity_point_bit[8];
+	LIST_SEGMENT(humidity_point_bit, data, 8, 16);
+	int temperature_bit[8];
+	LIST_SEGMENT(temperature_bit, data, 16, 24);
+	int temperature_point_bit[8];
+	LIST_SEGMENT(temperature_point_bit, data, 24, 32);
+	int check_bit[8];
+	LIST_SEGMENT(check_bit, data, 32, 40);
+	int humidity = 0;
+	int humidity_point = 0;
+	int temperature = 0;
+	int temperature_point = 0;
+	int check = 0;
+	for (int i = 0; i < 8; i++) {
+		humidity += pow(humidity_bit[i] * 2, 7 - i);
+		humidity_point += pow(humidity_point_bit[i] * 2, 7 - i);
+		temperature += pow(temperature_bit[i] * 2, 7 - i);
+		temperature_point += pow(temperature_point_bit[i] * 2, 7 - i);
+		check += pow(check_bit[i] * 2, 7 - i);
 	}
+	int tmp = humidity + humidity_point + temperature + temperature_point;
+	if (check == tmp) {
+		printf("temperature : %d, humidity : %d\n", temperature, humidity);
+	}
+	else {
+		printf("wrong\n");
+		printf("temperature : %d, humidity : %d check : %d tmp : %d\n", temperature, humidity, check, tmp);
+	}
+	// I dont think I need to clean up
 }
-
-humidity_bit = data[0:8]
-humidity_point_bit = data[8:16]
-temperature_bit = data[16:24]
-temperature_point_bit = data[24:32]
-check_bit = data[32:40]
-
-humidity = 0
-humidity_point = 0
-temperature = 0
-temperature_point = 0
-check = 0
-
-for i in range(8):
-	humidity += humidity_bit[i] * 2 ** (7 - i)
-	humidity_point += humidity_point_bit[i] * 2 ** (7 - i)
-	temperature += temperature_bit[i] * 2 ** (7 - i)
-	temperature_point += temperature_point_bit[i] * 2 ** (7 - i)
-	check += check_bit[i] * 2 ** (7 - i)
-
-tmp = humidity + humidity_point + temperature + temperature_point
-
-if check == tmp:
-	print ("temperature : ", temperature, ", humidity : " , humidity)
-else:
-	print ("wrong")
-	print ("temperature : ", temperature, ", humidity : " , humidity, " check : ", check, " tmp : ", tmp)
-
-GPIO.cleanup()
