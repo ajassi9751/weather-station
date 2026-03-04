06792ed79343b1a998a4363ad30a27e701e5f6a0
ajassi9751 <ajassi9751@gmail.com>
2026-03-04 14:49:08 +0000

    Made nix run and build work

--- flake.nix
+++ flake.nix
@@ -8,6 +8,13 @@
   outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
     armpkgs = nixpkgs.legacyPackages.arm-linux;
+    run64 = pkgs.writeShellApplication {
+      name = "run64";
+      runtimeInputs = with pkgs; [ cowsay ];
+      text = ''
+        ${pkgs.cowsay}/bin/cowsay "Hi from nix run"
+      '';
+    };
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       # Starts zsh as the default shell
@@ -22,6 +29,16 @@
         zsh
       ];
     };
+ 
+    packages.x86_64-linux.default = pkgs.writeShellScriptBin "default" ''
+      ${pkgs.cowsay}/bin/cowsay "Hello from a Nix build!"
+    '';
+
+    apps.x86_64-linux.default = {
+      program = "${run64}/bin/run64";
+      type = "app";
+    };
+
     # Use for raspberry pi
     devShells.arm-linux.default = pkgs.mkShell {
       # Starts zsh as the default shell
